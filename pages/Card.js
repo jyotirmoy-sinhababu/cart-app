72e74236ee37c221958bd335cea72cd6fa4edb66
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-01-07 19:07:02 +0530

    increment and decrement button added remove button added

--- pages/Card.js
+++ pages/Card.js
@@ -1,9 +1,40 @@
-const Card = ({ data }) => {
+import { useState } from 'react';
+
+const Card = ({ data, handleRemove }) => {
+  let [count, setCount] = useState(1);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    if (count != 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div>
-      <p>{data.title}</p>
-      <p>{data.quantity}</p>
-      <p>{data.price}</p>
+      <div>
+        {' '}
+        <p>{data.title}</p>
+        <p>{data.quantity}</p>
+        <p>{data.price * count}</p>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <button onClick={decrementCount}>-</button>
+        {count ? <p>{count}</p> : null}
+        <button onClick={incrementCount}>+</button>
+      </div>
+      <div>
+        <button
+          onClick={(id) => {
+            handleRemove(data.id);
+          }}
+        >
+          Remove
+        </button>
+      </div>
     </div>
   );
 };
--- pages/MainPage.js
+++ pages/MainPage.js
@@ -14,13 +14,22 @@ const MainPage = () => {
     });
   }, []);
 
+  const handleRemove = (id) => {
+    if (apiData) {
+      const filteredData = apiData.filter((item) => {
+        return item.id != id;
+      });
+      setApiData(filteredData);
+    }
+  };
+
   return (
     <>
       {apiData?.map((item) => {
         return (
           <div key={item.id}>
             {' '}
-            <Card data={item} />
+            <Card data={item} handleRemove={handleRemove} />
           </div>
         );
       })}
